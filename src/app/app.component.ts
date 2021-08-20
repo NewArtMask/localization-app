fde16b9ae92188f4ac27330e7717d8f6f54fb7e2
Oleg Postolovskyi <newartmask@gmail.com>
2021-08-20 18:21:23 +0300

    My localized app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
+
+interface Locale {
+  localeCode: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'localization-app';
+  locales: Locale[] = [
+    { localeCode: 'en-US', label: 'English' },
+    { localeCode: 'fr', label: 'Français' },
+  ];
+  name: string = 'John';
+  count: number = 5;
+  gender: string = 'male';
+  amount: number = 7.5;
+  currentDate: number = Date.now();
+
+  company: string = 'Localizely';
+  footerMessage: string = $localize`:Component argument message example@@message.component-argument:Made with ❤️ by ${this.company}`;
+
+  constructor(@Inject(LOCALE_ID) public locale: string) { }
 }
